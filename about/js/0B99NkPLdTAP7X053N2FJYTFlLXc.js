30c14f3d0f6b2e7e97bb46c92d31c9073363de5d
Agung Pambudi <agung@qasico.com>
2016-09-19 19:00:50 +0700

    fixing simple-line-font and script

--- about/js/0B99NkPLdTAP7X053N2FJYTFlLXc.js
+++ about/js/0B99NkPLdTAP7X053N2FJYTFlLXc.js
@@ -3,9 +3,9 @@
 //========================
 $(window).load(function() { // makes sure the whole site is loaded
 	$('#status').fadeOut(); // will first fade out the loading animation
-	$('#preloader').delay(350).fadeOut('slow');
+	$('#preloader').delay(150).fadeOut('slow');
     // will fade out the white DIV that covers the website.
-	$('body').delay(350).css({'overflow':'hidden'}); // hidden is the solution for white space after footer
+	$('body').delay(150).css({'overflow':'hidden','white-space':'nowrap'}); // hidden is the solution for white space after footer
 })
 //========================
 //CUSTOM SCROLLBAR
